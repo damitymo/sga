55eb712eb86744490b9b8d8a68d8f91b8b57b670
damitymo <tymoszukdamiandejesus@gmail.com>
2026-04-02 18:03:49 -0300

    fix agents module repositories

--- src/agents/agents.module.ts
+++ src/agents/agents.module.ts
@@ -5,11 +5,20 @@ import { AgentsController } from './agents.controller';
 import { AgentsService } from './agents.service';
 import { Agent } from './entities/agent.entity';
 import { AgentAssignment } from '../assignments/entities/agent-assignment.entity';
+import { AttendanceRecord } from '../attendance/entities/attendance-record.entity';
+import { RevistaRecord } from '../revista/entities/revista-record.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Agent, AgentAssignment])],
+  imports: [
+    TypeOrmModule.forFeature([
+      Agent,
+      AgentAssignment,
+      AttendanceRecord,
+      RevistaRecord,
+    ]),
+  ],
   controllers: [AgentsController],
   providers: [AgentsService],
-  exports: [AgentsService],
+  exports: [AgentsService, TypeOrmModule],
 })
 export class AgentsModule {}
